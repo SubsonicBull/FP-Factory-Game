853400e9f99a3fcf51b47003a1ceea6b39db03e4
SubsonicBull <97363116+SubsonicBull@users.noreply.github.com>
2022-04-05 21:48:33 +0200

    v-1.13

---
+++ Assets/Drill.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drill : MonoBehaviour
+{
+    public Animator animator;
+
+    void Start()
+    {
+        animator.SetTrigger("drillanddrillbittrig");
+    }
+
+    
+}
