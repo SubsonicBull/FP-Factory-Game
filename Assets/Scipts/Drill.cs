6715fab7de9d43b1cd3b429b41c5a642bb60db61
ResinDh <ferizi10rasim@gmail.com>
2022-04-05 22:55:49 +0200

    v1.15

--- Assets/Scipts/Drill.cs
+++ Assets/Scipts/Drill.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class Drill : MonoBehaviour
 {
     public Animator animator;
+    public float yOffset;
 
     void Start()
     {
         animator.SetTrigger("drillanddrillbittrig");
+        transform.Translate(new Vector3(0, yOffset, 0));
     }
 
     
