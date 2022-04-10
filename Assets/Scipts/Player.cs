02ec0e478a6ca2effe6394501c0e124643cfc4c7
SubsonicBull <97363116+SubsonicBull@users.noreply.github.com>
2022-04-10 16:35:08 +0200

    v-23
    we got pipes!!!!

--- Assets/Scipts/Player.cs
+++ Assets/Scipts/Player.cs
@@ -17,18 +17,25 @@ public class Player : MonoBehaviour
     public GameObject cantPlaceHereText;
     public GameObject flashlight;
     public GameObject buildUI;
+    public GameObject pipe;
     public LayerMask ground;
+    public LayerMask machine;
     public bool buildmode;
+    public Vector3 pipeOffset;
     Vector3 velocity;
     bool isGrounded;    
     int selectediventorySlot;    
-    RaycastHit hit;       
+    RaycastHit hit;
+    RaycastHit machinehit;
     Vector3 calculatedGridPos;
     Vector2 calculatedGridPos2d;
     public Image[] inventorySlots = new Image[5];
     public GameObject[] placableObjects = new GameObject[5];
     GameObject lastInstantiatedObject;
     List<Vector2> positionsOfPlacedObjects = new List<Vector2>();
+    GameObject pipeOutput;
+    GameObject pipeInput;
+    GameObject lastInstantiatedPipe;
 
 
     private void Start()
@@ -39,6 +46,8 @@ public class Player : MonoBehaviour
         flashlight.SetActive(false);
         buildmode = false;
         buildUI.SetActive(false);
+        pipeInput = null;
+        pipeOutput = null;
     }
 
     void Update()
@@ -124,6 +133,31 @@ public class Player : MonoBehaviour
                         cantPlaceHereText.SetActive(true);
                         Invoke("CantPlaceTextMethod", 2.0f);
                     }
+                }
+            }
+        }
+        //Pipes
+
+        if (Input.GetKeyDown("1") && buildmode)
+        {
+            if (Physics.Raycast(cam.transform.position,cam.transform.forward, out hit, 20, machine))
+            {
+                if (pipeInput == null)
+                {
+                    pipeInput = hit.collider.gameObject;
+                }
+                else
+                {
+                    if (hit.collider.gameObject != pipeInput)
+                    {
+                        pipeOutput = hit.collider.gameObject;
+                        lastInstantiatedPipe = Instantiate(pipe, pipeInput.transform.position + pipeOffset, Quaternion.Euler(0, 0, 0));
+                        lastInstantiatedPipe.transform.LookAt(pipeOutput.transform.position + pipeOffset);
+                        lastInstantiatedPipe.transform.position += new Vector3((pipeOutput.transform.position.x - pipeInput.transform.position.x)/2, (pipeOutput.transform.position.y - pipeInput.transform.position.y)/2, (pipeOutput.transform.position.z - pipeInput.transform.position.z)/2);
+                        lastInstantiatedPipe.transform.localScale = new Vector3(1f, 1f, Vector3.Distance(pipeInput.transform.position, pipeOutput.transform.position)/2);
+                        pipeInput = null;
+                        pipeOutput = null;
+                    }                   
                 }
             }
         }
